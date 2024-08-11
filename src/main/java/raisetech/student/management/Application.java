cfe7ba9dafbf890bc4e20dc18ebf079a8e236ea6
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-08-11 18:50:07 +0900

    課題20_初回提出

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,30 +1,59 @@
 package raisetech.student.management;
 
-import java.util.HashMap;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
-	private final HashMap<String, String> info = new HashMap<>();
+
+	@Autowired
+	private StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/info")
-	public HashMap<String, String> getInfo() {
-		return info;
+	@GetMapping("/student")
+	public String getStudent(@RequestParam("name") String name) {
+		Student student = studentRepository.searchByName(name);
+		return student.getName() + " " + student.getAge() + "歳";
 	}
 
-	@PostMapping("/info")
-	public void addInfo(int id, String name) {
-		info.put(String.valueOf(id), name);
+	@PostMapping("/student")
+	public void registerStudent(String name, int age) {
+		studentRepository.registerStudent(name, age);
 	}
+
+	@PatchMapping("/student")
+	public void updateStudentAge(String name, int age) {
+		studentRepository.updateStudent(name, age);
+	}
+
+	@DeleteMapping("/student")
+	public void deleteStudent(String name) {
+		studentRepository.deleteStudent(name);
+	}
+
+//	課題用：全生徒の情報取得
+	@GetMapping("/allStudents")
+	public String getAllStudents() {
+		List<Student> students = studentRepository.getAllStudents();
+		String result = "";
+		for (Student student : students) {
+			result += student.getName() + " " + student.getAge() + "歳\n";
+		}
+		return result;
+	}
+
 }
 
 
