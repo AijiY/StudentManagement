6ab94d859790d5f53cdacd2a0d4d521a425ae8d1
Aiji Yamamura <j2yamimjr3i@gmail.com>
2024-08-14 10:46:11 +0900

    課題23_提出

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -4,11 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,41 +13,21 @@ public class Application {
 
 	@Autowired
 	private StudentRepository studentRepository;
+	@Autowired
+	private StudentCourseRepository studentCourseRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/student")
-	public String getStudent(@RequestParam("name") String name) {
-		Student student = studentRepository.searchByName(name);
-		return student.getName() + " " + student.getAge() + "歳";
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return studentRepository.search();
 	}
 
-	@PostMapping("/student")
-	public void registerStudent(String name, int age) {
-		studentRepository.registerStudent(name, age);
-	}
-
-	@PatchMapping("/student")
-	public void updateStudentAge(String name, int age) {
-		studentRepository.updateStudent(name, age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name) {
-		studentRepository.deleteStudent(name);
-	}
-
-//	課題用：全生徒の情報取得
-	@GetMapping("/allStudents")
-	public String getStudents() {
-		List<Student> students = studentRepository.getStudents();
-		String result = "";
-		for (Student student : students) {
-			result += student.getName() + " " + student.getAge() + "歳\n";
-		}
-		return result;
+	@GetMapping("/studentCourseList")
+	public List<StudentCourse> getStudentCourseList() {
+		return studentCourseRepository.search();
 	}
 
 }
